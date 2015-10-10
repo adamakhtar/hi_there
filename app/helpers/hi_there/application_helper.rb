eff783ee69c258171136b27481a6b8d6220ccf1a
Adam Akhtar <adam.akhtar@gmail.com>
2015-10-10 22:56:34 +0900

    push all nav links to the right and remove active link class

--- app/helpers/hi_there/application_helper.rb
+++ app/helpers/hi_there/application_helper.rb
@@ -3,9 +3,5 @@ module HiThere
     def body_classes
       "hi_there_#{controller_name} hi_there_#{controller_name}_#{action_name}"
     end
-
-    def nav_link_class(link_name)
-      "active" if controller_name == link_name.to_s
-    end
   end
 end
