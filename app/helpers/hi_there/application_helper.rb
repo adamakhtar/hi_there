c65556130867b90ba41d3238ddc7ea125e5bfe08
Adam Akhtar <adam.akhtar@gmail.com>
2015-10-11 18:36:38 +0900

    email edit button is disabled instead of hidden when course is active

--- app/helpers/hi_there/application_helper.rb
+++ app/helpers/hi_there/application_helper.rb
@@ -3,5 +3,20 @@ module HiThere
     def body_classes
       "hi_there_#{controller_name} hi_there_#{controller_name}_#{action_name}"
     end
+
+    def link_to_with_state(link:, path:, is_disabled: false, is_active: false, is_dangerous: false, confirm_message: nil, **options)
+      options.reverse_merge!({class: ""})
+      if is_disabled
+        path = 'javascript:void(0)'
+        options[:class] += " disabled"        
+      elsif is_active
+        path = 'javascript:void(0)'
+        options[:class] += " active"        
+      end
+
+      options[:data] = { confirm: confirm_message } if is_dangerous 
+
+      link_to link, path, options
+    end
   end
 end
