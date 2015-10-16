bc276d9069266e3ed11ea9dd6a2d25ecadbe058f
Adam Akhtar <adam.akhtar@gmail.com>
2015-10-16 09:24:03 +0900

    remove title validation from course
    could be possible admin wants to create a new version of
    an older sunset course.

--- app/models/hi_there/course.rb
+++ app/models/hi_there/course.rb
@@ -5,7 +5,7 @@ module HiThere
     has_many :emails, -> { order(issue_number: :asc) }
     has_many :subscriptions
 
-    validates :title, presence: true, uniqueness: true
+    validates :title, presence: true
     validates :name, format: {with: /\A[a-z_\d]+\z/}
     
     workflow do
