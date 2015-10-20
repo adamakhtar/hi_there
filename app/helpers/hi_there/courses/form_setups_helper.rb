a4fb5c47326761664c45298a3263621a3ffc9d13
Adam Akhtar <adam.akhtar@gmail.com>
2015-10-20 22:37:10 +0900

    show new way to wire forms in form_setups

--- app/helpers/hi_there/courses/form_setups_helper.rb
+++ app/helpers/hi_there/courses/form_setups_helper.rb
@@ -5,16 +5,14 @@ module HiThere
         # TOFIX For some reason we need a newline after herdoc otherwise
         # indentation is broken when rendered.
         string = <<-DOC.strip_heredoc
-        
-        <h1>My home page</h1>
         <% if @course and @course.opened? %>
           <h2><%= @course.title %></h2>
           <p><%= @course.description %></p>
-          <%= simple_form_for @subscription, url: hi_there.subscriptions_path do |f| %>
+          <%= form_tag hi_there.subscriptions_path do |f| %>
             <%= text_field_tag :secret_field %>
-            <%= f.input :email %>
-            <%= f.input :course_id, as: :hidden %>
-            <%= f.submit "Sign up" %>
+            <%= text_field_tag :email %>
+            <%= hidden_field_tag :course_id, @course.id %>
+            <%= submit_tag "Sign up" %>
           <% end %>
         <% end %>
         DOC
@@ -25,7 +23,6 @@ module HiThere
         # TOFIX For some reason we need a newline after herdoc otherwise
         # indentation is broken when rendered.
         string = <<-DOC.strip_heredoc
-
           class SomeController < ApplicationController          
             def some_action
               @course = HiThere::Course.where(name: '#{course.name}').take
